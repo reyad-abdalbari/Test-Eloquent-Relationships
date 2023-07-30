638c0b05ab91fcb096c8d701e2d3e6ecac7d180b
reyad-abdalbari <140594502+reyad-abdalbari@users.noreply.github.com>
2023-07-31 01:38:46 +0300

    Test-Eloquent-Relationships

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+
+class CountryController extends Controller
+{
+    public function index()
+    {
+        // TASK: load the relationship average of team size
+        $countries = Country::withAvg('teams','size')->get();;
+
+        $countries = Country::all();
+
+        return view('countries.index', compact('countries'));
+    }
+}
